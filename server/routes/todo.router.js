21d3a6065a95980d929b1514dff65c74ca0c1ade
Pouapheng Yang <phewy06@yahoo.com>
2021-07-03 22:38:38 -0500

    router lines

--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -9,4 +9,28 @@ const config = {
     host: 'localhost',
     port: 5432,
     max: 10,
-}
+};
+
+const pool = new Pool(config);
+
+pool.on('connect', (client) => {
+    console.log('PostgeSQL connected');
+});
+
+pool.on('error', (err, client) => {
+    console.log('Unexpected error, client nowhere to be found', err);
+});
+
+toDoRouter.get('/', (req, res) =>{
+    let qText = 'Select * FROM "todolist" ORDER BY completed ASC;';
+
+    pool.query(qtext)
+        .then(result => {
+            res.send(result.rows);
+        })
+        .catch(err => {
+            console.log('Error trying to get TO DO list', err);
+            res.sendStatus(500);
+        });
+});
+
